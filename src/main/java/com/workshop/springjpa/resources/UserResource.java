fd7ea04b13648d391998a2793077b4e0402d968a
DionatanLeao <dionatan.ti@outlook.com>
2021-07-25 09:11:42 -0300

    User entity and resource

---
+++ src/main/java/com/workshop/springjpa/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.workshop.springjpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workshop.springjpa.entities.User;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@email", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
