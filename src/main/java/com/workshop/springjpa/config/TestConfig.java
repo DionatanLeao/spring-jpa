5605a4b3ec53169bb3549b21dc199c71225a7288
DionatanLeao <dionatan.ti@outlook.com>
2021-09-13 20:07:08 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/workshop/springjpa/config/TestConfig.java
+++ src/main/java/com/workshop/springjpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.workshop.springjpa.entities.Category;
 import com.workshop.springjpa.entities.Order;
 import com.workshop.springjpa.entities.OrderItem;
+import com.workshop.springjpa.entities.Payment;
 import com.workshop.springjpa.entities.Product;
 import com.workshop.springjpa.entities.User;
 import com.workshop.springjpa.entities.enums.OrderStatus;
@@ -88,21 +89,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
 		
+		o1.setPayment(pay1);
 		
-		
-		
-		 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		orderRepository.save(o1);
 		
 
 	}
