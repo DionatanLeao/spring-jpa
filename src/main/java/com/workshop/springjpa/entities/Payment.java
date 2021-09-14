b9415e8129524c7f52dc99474f2edb9ed088bb27
DionatanLeao <dionatan.ti@outlook.com>
2021-09-14 17:16:56 -0300

    Subtotal & Total methods

--- src/main/java/com/workshop/springjpa/entities/Payment.java
+++ src/main/java/com/workshop/springjpa/entities/Payment.java
@@ -12,6 +12,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /***
  * 
  * @author Dionatan Leão
@@ -28,6 +30,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
