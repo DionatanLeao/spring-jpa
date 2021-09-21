b53e7dff5c709d226a04daec24f869b8c68c9f88
DionatanLeao <dionatan.ti@outlook.com>
2021-09-21 10:03:42 -0300

    Exception handling - findById

---
+++ src/main/java/com/workshop/springjpa/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.workshop.springjpa.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.workshop.springjpa.services.exceptions.ResourceNotFoundException;
+
+/***
+ * 
+ * @author Dionatan Leão
+ *
+ */
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/workshop/springjpa/services/UserService.java
+++ src/main/java/com/workshop/springjpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.workshop.springjpa.entities.User;
 import com.workshop.springjpa.repositories.UserRepository;
+import com.workshop.springjpa.services.exceptions.ResourceNotFoundException;
 
 /***
  * 
@@ -27,7 +28,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
